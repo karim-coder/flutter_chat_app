23aa0bed4e429451a2f2ae4dadc0e80e75185b86
karim-coder <ab.amiri484@gmail.com>
2023-12-17 01:17:34 +0530

    Message screen and implementation done

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,21 +9,25 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Chat'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter Chat'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.primary),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ],
+        ),
+        body: const Column(
+          children: [
+            Expanded(
+              child: ChatMessages(),
+            ),
+            NewMessage()
+          ],
+        ));
   }
 }
